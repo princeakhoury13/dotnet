28b8623412c8ae677098e0a3ed684146a6cdd5c2
princeakhoury13 <princepraveen130509@gmail.com>
2023-02-08 07:48:09 +0530

    added: all files

---
+++ LeapYear.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class LeapYear
+    {
+        public static void IsLeapYear()
+        {
+            Console.Write("Enter a year: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            bool leap = false;
+            if (year % 400 == 0)
+            {
+                leap = true;
+            }
+            else if (year % 100 == 0)
+            {
+                leap = false;
+            }
+            else if (year % 4 == 0)
+            {
+                leap = true;
+            }
+            Console.WriteLine(leap);
+        }
+    }
+}
---
+++ Power.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Power
+    {
+        public static void Exponent()
+        {
+            Console.WriteLine("Enter the value of n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            
+            if (n >= 0 && n < 31)
+            {
+                Console.WriteLine("Power of 2 until " + n);
+                int pow = 1;
+                for (int i = 1; i <= n; i++)
+                {
+                    pow = pow * 2;
+                    Console.WriteLine(pow);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid Number");
+            }
+        }
+    }
+}
